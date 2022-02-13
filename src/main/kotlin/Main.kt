144bf6f9009830232fd5689aa2f689dda2b9c963
mercy-yugi <mercyyugi@gmail.com>
2022-02-13 15:31:47 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main(args: Array<String>) {
+    //student name
+    val fullname: String= "Mercy Yugi"
+    println(fullname)
+    //student age
+    var age: Byte= 23
+    println(age)
+//student phone number
+    val phonenumber :String= "07466656456"
+    println(phonenumber)
+    //student weight in kg
+    val weight: Float=56.2F
+    println(weight)
+    //citizenship =kenyan
+    val  kenyan: Boolean = true
+    println(kenyan)
+
+}
